8bc588f2ae4faa24575c36c5a5e3777e1081f696
jogaproger <jogaproger@gmail.com>
2015-03-20 00:06:08 +0100

    first commit

---
+++ src/Skeleton.java
@@ -0,0 +1,50 @@
+
+public class Skeleton {
+
+	
+	public static void main( String[] args )
+	{
+		try{
+			new Skeleton().A();
+		}
+		catch( Exception e )
+		{
+			e.printStackTrace();	
+		}
+	}
+	
+	void B(){
+		Logger.printCall(this, "random string as B's param");
+		Logger.printCallEnd();		
+	}
+	
+	void C(){
+		Logger.printCall(this);
+		
+		D(this, 3);
+		
+		Logger.printCallEnd();		
+	}
+	
+	void D( Skeleton param, int i )
+	{
+		// Mindig thissel kezdõdik
+		// az elsõ paraméter egy Skeletonobjektum, aminek neve van ezért simán átadjuk
+		// A második egy sima érték, ezt stringgé konvertáljuk és úgy adjuk át
+		Logger.printCall(this, param, ""+i );
+		
+		Logger.print("Egyéb szöveg kiírása D-bõl");
+		
+		Logger.printCallEnd();
+	}
+	
+	void A(){
+		Logger.printCall(this);
+		
+		B();
+		C();
+		
+		Logger.printCallEnd();		
+	}
+	
+}
