ef8654ae1b3ff163dc6a07f3491acf9495c012ba
jogaproger <jogaproger@gmail.com>
2015-03-20 09:31:16 +0100

    skeleton package létrehozása

--- src/modell/Jatek.java
+++ src/modell/Jatek.java
@@ -1,5 +1,13 @@
 package modell;
 
+
 public class Jatek {
-	int csinaltamegyvaltozot;
+	private static Jatek instance = null;
+	
+	void ujJatek( String palya, int jatekosnum ){
+		Logger.printCall(this, palya, ""+jatekosnum);
+		
+		Logger.printCallEnd();
+	}
+	
 }
