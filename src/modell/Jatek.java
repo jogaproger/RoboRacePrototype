3aca82ad6ddec2ec5158d584e837e0e5797f4e53
jogaproger <jogaproger@gmail.com>
2015-03-20 09:45:01 +0100

    Jatek osztaly alakul

--- src/modell/Jatek.java
+++ src/modell/Jatek.java
@@ -1,11 +1,34 @@
 package modell;
+import java.util.ArrayList;
 
+import skeleton.Logger;
 
 public class Jatek {
-	private static Jatek instance = null;
+	private static Jatek instance = new Jatek();
 	
-	void ujJatek( String palya, int jatekosnum ){
+	private Ranglista ranglista = new Ranglista();
+	private Jatekos[] jatekosok;
+	private ArrayList<JatekObj> objects;
+	private Palya palya;
+	private boolean endflag;
+	private int ido;
+	
+	public static Jatek getInstance(){
+		return instance;		
+	}
+	
+	public void ujJatek( String palya, int jatekosnum ){
 		Logger.printCall(this, palya, ""+jatekosnum);
+		
+		jatekosok = new Jatekos[jatekosnum];
+		objects = new ArrayList<JatekObj>();
+		
+		Logger.printCallEnd();
+	}
+	
+	public void addJatekObj( JatekObj j ){
+		Logger.printCall(this, j);
+		
 		
 		Logger.printCallEnd();
 	}
