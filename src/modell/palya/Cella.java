954f6a94597225ddb06b1ac56b83db1d4dd9172a
mynameistrish <corydoras92@gmail.com>
2015-03-20 19:03:38 +0100

    palya.packkage initial allapot

--- src/modell/palya/Cella.java
+++ src/modell/palya/Cella.java
@@ -1,5 +1,48 @@
 package modell.palya;
 
+import modell.jatekobj.JatekObj;
+import modell.jatekobj.Robot;
+
 public class Cella {
 
+    private int x;
+    private int y;
+    private JatekObj[] o;
+    private Palya palya;
+    
+    Cella(int x, int y) {
+
+    }
+    /**
+     * Kovetkezo cella lekerdezese,sebessegtol fuggoen
+     * @param s Aktualis sebesseg
+     * @return 
+     */
+    public Cella getKov(Sebesseg s) {
+        int resultx = 0;
+        int resulty = 0;
+        Cella c = new Cella(resultx, resulty);
+        return c;
+    }
+    /**
+     * Jatekobjektum hozzaadasa a cellahoz
+     * @param j Lerakando jatekobjektum
+     */
+    public void add(JatekObj j) {
+
+    }
+    /**
+     * Jatekobjektum eltavolitasa a cellarol
+     * @param j Eltavolitando jatekobjektum
+     */
+    public void remove(JatekObj j) {
+
+    }
+    /**
+     * Robot ralep a cellara
+     * 
+     */
+    public void ralep(Robot r) {
+
+    }
 }
--- src/modell/palya/Sebesseg.java
+++ src/modell/palya/Sebesseg.java
@@ -2,4 +2,44 @@ package modell.palya;
 
 public class Sebesseg {
 
+    int fel;
+    int jobbra;
+
+     /**
+     *  Sebesseg nagysaganak lekerdezese
+     * 
+     */
+    public int getNagysag() {
+        return 0;
+    }
+        
+    /**
+    *  Irany modositasa 
+    * 
+    */
+    public void modosit(Irany i) {
+
+    }
+    /**
+    *  Nulla-e az aktualis sebesseg?
+    * 
+    */
+    public Boolean isNulla() {
+        return false;
+    }
+    /**
+    *  Sebesseg felezese
+    * 
+    */
+    public void felez() {
+
+    }
+    /**
+    *  Sebesseg nullara allitasa
+    * 
+    */
+    public void setNulla() {
+
+    }
+
 }
