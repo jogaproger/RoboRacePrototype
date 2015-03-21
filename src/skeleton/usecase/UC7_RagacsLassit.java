9429f5adcb20336dbae27e4e2ff45ad7cc4e8c7a
jogaproger <jogaproger@gmail.com>
2015-03-21 16:11:00 +0100

    UseCase osztályok létrehozása

---
+++ src/skeleton/usecase/UC7_RagacsLassit.java
@@ -0,0 +1,17 @@
+package skeleton.usecase;
+
+public class UC7_RagacsLassit extends UseCase {
+
+	@Override
+	public void execute() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public String getName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ src/skeleton/usecase/UC8_BlokkElpusztit.java
@@ -0,0 +1,17 @@
+package skeleton.usecase;
+
+public class UC8_BlokkElpusztit extends UseCase {
+
+	@Override
+	public void execute() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public String getName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
